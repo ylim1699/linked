28552609fbfd794603ffd4c45a736acdd5ff5f93
ylim1699 <ylim.0348@gmail.com>
2025-11-17 16:04:24 -0700

    feat: added dist folder

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-// import { dirname, resolve } from "path";
-// import { defineConfig } from "vite";
-// import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
 
-// const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// export default defineConfig({
-//   root: "src/",
+export default defineConfig({
+  root: "src/",
 
-//   build: {
-//     outDir: "../dist",
-//     rollupOptions: {
-//       input: {
-//         main: resolve(__dirname, "src/index.html"),
-//       }
-//     }
-//   }
-// });
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+      }
+    }
+  }
+});
