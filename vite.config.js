eeee0eed7035ceb11898b15ce6ac329898ac8200
ylim1699 <ylim.0348@gmail.com>
2025-12-01 16:15:38 -0700

    .

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        discover: resolve(__dirname, "src/discover.html"),
+        calendar: resolve(__dirname, "src/calendar.html"),
       }
     }
   }
