d4652bafdf54020f56f4f0ab7db193be3d1a2017
ylim1699 <ylim.0348@gmail.com>
2025-12-07 20:57:31 -0700

    image uploaded to live server

--- src/js/clubs.mjs
+++ src/js/clubs.mjs
@@ -1,4 +1,14 @@
-import cooking from '../images/cooking.jpg'
+import cooking from '../images/cooking.jpg';
+import country from '../images/countrydance.jpg';
+import dungeon from '../images/dungeonsanddragons.webp';
+import trump from '../images/tump.jpg';
+import biden from '../images/joe.jpg';
+import rocket from '../images/rocket.webp';
+import laser from '../images/lasertag.jpg';
+import latin from '../images/latindancing.jpg';
+import roller from '../images/rollerskate.jpg';
+import ropes from '../images/ropes.jpg';
+
 
 export const clubsData = [
     {
@@ -6,7 +16,7 @@ export const clubsData = [
         name: "Country Dancing Club",
         description: "A community for students interested in country dancing",
         members: 2156,
-        imageSrc: `./images/countrydance.jpg`,
+        imageSrc: country,
         fallbackText: "No country dancing this semester",
         joinLink: "#join-country",
         link: "https://www.byui.edu/calendar/weekly-activities/country-dancing-2025-12-10-11520403-1",
@@ -28,7 +38,7 @@ export const clubsData = [
         name: "Donald Trump Club",
         description: "A community for students interested in Making America great again.",
         members: 7,
-        imageSrc: `./images/tump.jpg`,
+        imageSrc: trump,
         fallbackText: "no trump club this semester",
         joinLink: "#join-trump",
         link: "https://www.foxnews.com/",
@@ -39,7 +49,7 @@ export const clubsData = [
         name: "D&D",
         description: "A community for students interested in dungeons and dragons.",
         members: 120,
-        imageSrc: `./images/dungeonsanddragons.webp`,
+        imageSrc: dungeon,
         fallbackText: "no D&D club this semester",
         joinLink: "#join-d&d",
         link: "https://www.byui.edu/calendar/weekly-activities/game-night-dungeons-and-dragons-2025-12-11-11520404-1",
@@ -50,7 +60,7 @@ export const clubsData = [
         name: "Joe Biden club",
         description: "A community for students interested in curing dementia and alzheimer's disease.",
         members: 56,
-        imageSrc: `./images/joe.jpg`,
+        imageSrc: biden,
         fallbackText: "no Joe Biden club this semester",
         joinLink: "#join-biden",
         link: "https://www.cnn.com/",
@@ -61,7 +71,7 @@ export const clubsData = [
         name: "The Rocket club",
         description: "A community for students interested in building rockets",
         members: 666,
-        imageSrc: `./images/rocket.webp`,
+        imageSrc: rocket,
         fallbackText: "no Rocket club this semester",
         joinLink: "#join-rocket",
         link: "https://www.youtube.com/watch?v=Fe4PEnyVr9E",
@@ -72,7 +82,7 @@ export const clubsData = [
         name: "Laser Tag Club",
         description: "Have fun playing laser tag anytime for free!",
         members: 520,
-        imageSrc: `./images/lasertag.jpg`,
+        imageSrc: laser,
         fallbackText: "Laser tag not available this semester",
         joinLink: "#join-laser",
         link: "https://www.byui.edu/calendar/weekly-activities/home-evening-laser-tag-2025-12-08-11520398-1",
@@ -83,7 +93,7 @@ export const clubsData = [
         name: "Latin Dance Club",
         description: "A community for students interested in latin dancing",
         members: 85,
-        imageSrc: `./images/latindancing.jpg`,
+        imageSrc: latin,
         fallbackText: "no latin dancing this semester",
         joinLink: "#join-latin",
         link: "https://www.byui.edu/calendar/weekly-activities/latin-dance-2025-12-09-11520402-1",
@@ -94,7 +104,7 @@ export const clubsData = [
         name: "Roller Skating Club",
         description: "Group up students who enjoy roller skating",
         members: 178,
-        imageSrc: `./images/rollerskate.jpg`,
+        imageSrc: roller,
         fallbackText: "Roller Skating Club not available for this semester",
         joinLink: "#join-roller",
         link: "https://www.byui.edu/skateway-roller-skating/",
@@ -105,7 +115,7 @@ export const clubsData = [
         name: "Ropes course Club",
         description: "A community for students interested in various ropes course activities",
         members: 356,
-        imageSrc: `./images/ropes.jpg`,
+        imageSrc: ropes,
         fallbackText: "Ropes course Club not available for this semester",
         joinLink: "#join-ropes",
         link: "https://www.byui.edu/ropes-course/",
